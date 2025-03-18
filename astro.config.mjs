a8f280e9c49ddc5ead04df2957d6b55664a9423d
Kevine_dev <kevinarson_1996@yahoo.fr>
2025-03-19 00:54:33 +0100

    Features- add tag and Fix-bug config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,28 +1,52 @@
 // @ts-check
-import { defineConfig } from "astro/config";
-
-import tailwindcss from "@tailwindcss/vite";
-
-import icon from "astro-icon";
-
 import react from "@astrojs/react";
 import sanity from "@sanity/astro";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
+import { defineConfig } from "astro/config";
+import path from "path";
+
+import sitemap from "@astrojs/sitemap";
+
+import { SITE } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
+  prefetch: true,
+  site: SITE.website,
 
   integrations: [
     icon(),
     sanity({
       projectId: "zmv1ajh4",
       dataset: "production",
-      // Set useCdn to false if you're building statically.
       useCdn: false,
+      // Access the Studio on your.url/admin
+      studioBasePath: "/admin",
     }),
     react(),
+    sitemap(),
   ],
+  output: "server",
+  trailingSlash: "ignore",
+  vite: {
+    plugins: [tailwindcss()],
+    logLevel: "error",
+    define: {
+      __DATE__: `'${new Date()}'`,
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"), // Add this alias
+      },
+    },
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "github-light",
+        dark: "github-dark",
+      },
+    },
+  },
 });
