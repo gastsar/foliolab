f90180149d2d1cd215208883ceef30f23dc99f06
Kevine_dev <kevinarson_1996@yahoo.fr>
2025-03-24 14:54:44 +0100

    DELETE  coockier banner and delete icone skill about

--- src/data.js
+++ src/data.js
@@ -2,265 +2,219 @@
 export const skills = [
   {
     category: "Développement Frontend",
-    icon: "fa-solid fa-code",
     items: [
       {
         name: "JavaScript",
         level: 7,
-        icon: "fa-brands fa-js",
         expertise: "Intermediate",
       },
       {
         name: "TypeScript",
         level: 6,
-        icon: "fa-solid fa-code",
         expertise: "Intermediate",
       },
       {
         name: "TailwindCSS",
         level: 7,
-        icon: "fa-solid fa-palette",
         expertise: "Intermediate",
       },
       {
         name: "Sass",
         level: 6,
-        icon: "fa-brands fa-sass",
         expertise: "Intermediate",
       },
       {
         name: "Bootstrap",
         level: 7,
-        icon: "fa-brands fa-bootstrap",
         expertise: "Intermediate",
       },
       {
         name: "React",
         level: 7,
-        icon: "fa-brands fa-react",
         expertise: "Intermediate",
       },
       {
         name: "React Native",
         level: 5,
-        icon: "fa-solid fa-mobile-screen",
         expertise: "Beginner",
       },
       {
         name: "Next.js",
         level: 6,
-        icon: "fa-solid fa-arrow-right",
         expertise: "Intermediate",
       },
       {
         name: "Astro",
         level: 4,
-        icon: "fa-solid fa-star",
         expertise: "Beginner",
       },
     ],
   },
   {
     category: "Développement Backend",
-    icon: "fa-solid fa-server",
     items: [
       {
         name: "API REST",
         level: 6,
-        icon: "fa-solid fa-link",
         expertise: "Intermediate",
       },
       {
         name: "MySQL",
         level: 6,
-        icon: "fa-solid fa-database",
         expertise: "Intermediate",
       },
       {
         name: "Firebase",
         level: 5,
-        icon: "fa-solid fa-fire",
         expertise: "Beginner",
       },
       {
         name: "Prisma",
         level: 5,
-        icon: "fa-solid fa-gem",
         expertise: "Beginner",
       },
     ],
   },
   {
     category: "DevOps & Outils",
-    icon: "fa-solid fa-gears",
     items: [
       {
         name: "Git",
         level: 8,
-        icon: "fa-brands fa-git-alt",
         expertise: "Advanced",
       },
       {
         name: "GitHub",
         level: 8,
-        icon: "fa-brands fa-github",
         expertise: "Advanced",
       },
       {
         name: "GitLab",
         level: 6,
-        icon: "fa-brands fa-gitlab",
         expertise: "Intermediate",
       },
       {
         name: "CI/CD",
         level: 5,
-        icon: "fa-solid fa-infinity",
         expertise: "Beginner",
       },
       {
         name: "Docker",
         level: 4,
-        icon: "fa-brands fa-docker",
         expertise: "Beginner",
       },
       {
         name: "Jest",
         level: 5,
-        icon: "fa-solid fa-vial",
         expertise: "Beginner",
       },
     ],
   },
   {
     category: "Qualité & Performance",
-    icon: "fa-solid fa-bolt",
     items: [
       {
         name: "Responsive Design",
         level: 8,
-        icon: "fa-solid fa-expand",
         expertise: "Advanced",
       },
       {
         name: "Accessibilité",
         level: 6,
-        icon: "fa-solid fa-universal-access",
         expertise: "Intermediate",
       },
       {
         name: "Performance Web",
         level: 5,
-        icon: "fa-solid fa-gauge",
         expertise: "Beginner",
       },
     ],
   },
   {
     category: "Design & UI/UX",
-    icon: "fa-solid fa-pen-nib",
     items: [
       {
         name: "Figma",
         level: 6,
-        icon: "fa-brands fa-figma",
         expertise: "Intermediate",
       },
       {
         name: "Penpot",
         level: 5,
-        icon: "fa-solid fa-pen-ruler",
         expertise: "Beginner",
       },
       {
         name: "Design System",
         level: 6,
-        icon: "fa-solid fa-palette",
         expertise: "Intermediate",
       },
     ],
   },
-
   {
     category: "Outils de Développement",
-    icon: "fa-solid fa-toolbox",
     items: [
       {
         name: "VS Code",
         level: 8,
-        icon: "fa-solid fa-laptop-code",
         expertise: "Advanced",
       },
       {
         name: "Cursor",
         level: 6,
-        icon: "fa-solid fa-laptop-code",
         expertise: "Advanced",
       },
       {
         name: "Markdown",
         level: 7,
-        icon: "fa-solid fa-file-alt",
         expertise: "Intermediate",
       },
       {
         name: "Terminal",
         level: 6,
-        icon: "fa-solid fa-terminal",
         expertise: "Intermediate",
       },
       {
         name: "Postman",
         level: 7,
-        icon: "fa-solid fa-envelope-open-text",
         expertise: "Intermediate",
       },
       {
         name: "DevTools",
         level: 7,
-        icon: "fa-solid fa-tools",
         expertise: "Intermediate",
       },
     ],
   },
   {
     category: "Compétences Transversales",
-    icon: "fa-solid fa-rocket",
     items: [
       {
         name: "Communication",
         level: 7,
-        icon: "fa-solid fa-comments",
       },
       {
         name: "Collaboration",
         level: 7,
-        icon: "fa-solid fa-handshake",
       },
       {
         name: "Esprit d'équipe",
         level: 8,
-        icon: "fa-solid fa-people-group",
       },
       {
         name: "Résolution de problèmes",
         level: 7,
-        icon: "fa-solid fa-brain",
       },
       {
         name: "Adaptabilité",
         level: 8,
-        icon: "fa-solid fa-sync",
       },
       {
         name: "Autonomie",
         level: 8,
-        icon: "fa-solid fa-user-check",
       },
       {
         name: "Rigueur",
         level: 7,
-        icon: "fa-solid fa-ruler",
       },
     ],
   },
